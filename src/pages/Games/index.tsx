18893fac620d409699427ef6011b7b85939fea26
Ednaldo Gomes <egomes@metatron.com.br>
2024-10-31 14:20:24 -0300

    feat: ordena a data de limpeza de forma decrescente

--- src/pages/Games/index.tsx
+++ src/pages/Games/index.tsx
@@ -10,8 +10,34 @@ import { Game } from "../../shared/models/Games.js";
 
 const Games: React.FC = () => {
 
+    const monthLimit: number = 6;
+    const subtitle = `Frequência para Limpeza: ${monthLimit} meses`;
+    const today = new Date().toISOString();
     // const games = useSelector(selectGames);
     const [ games, setGames ] = useState<Game[]|null>(null);
+
+    const getDiffDays = (startDate: string, endDate: string): number => {
+        const partidaViagem = new Date(startDate);
+        const partidaPrimEtapa = new Date(endDate);
+        const timeDiff = Math.abs(partidaPrimEtapa.getTime() - partidaViagem.getTime());
+        const diffDays = Math.round(timeDiff / (1000 * 3600 * 24));
+        // const diffMinutes = Math.ceil(timeDiff / (1000 * 60));
+        return diffDays;
+    }
+
+    const getTimeSinceLastCleaning = (startDate: string, endDate: string) => {
+        const diff = getDiffDays(startDate, endDate);
+        if(diff < 30) {
+            return `${diff} dias`
+        }
+
+        return `${Math.floor(diff / 30)} meses e ${diff%30} dias`;
+    }
+
+    const checkLimit = (startDate: string, endDate: string): boolean => {
+        const diff = getDiffDays(startDate, endDate);
+        return Math.floor(diff / 30) >= monthLimit;
+    }
 
     useEffect(() => {
         const fetchGames = async () => {
@@ -27,7 +53,12 @@ const Games: React.FC = () => {
                     ...doc.data() as Game
                 });
             });
-            setGames(gameList);
+
+            const sortedGameList = gameList.sort(function (a,b) {
+                return new Date(a.cleaning_date)<new Date(b.cleaning_date)?-1:new Date(a.cleaning_date)>new Date(b.cleaning_date)?1:0;
+            })
+
+            setGames(sortedGameList);
             // setGames(prevState => [
             //     ...prevState || [],
             //     ...doc.data() as Game[]
@@ -40,6 +71,7 @@ const Games: React.FC = () => {
     return (
         <Container>    
             <h2>Últimas Limpezas Nos Board Games</h2>
+            <small>{subtitle}</small>
             <ul>
                 {(games as Game[])?.map((game: Game) => (
                     <li key={game.id}>
@@ -52,6 +84,9 @@ const Games: React.FC = () => {
                                 )
                                 : "N/A"
                             }
+                        </p>
+                        <p className={checkLimit(game.cleaning_date, today) ? "limit" : ""}>
+                            { getTimeSinceLastCleaning(game.cleaning_date, today) }
                         </p>
                     </li>
                 ))}
--- src/pages/Games/styles.ts
+++ src/pages/Games/styles.ts
@@ -5,6 +5,7 @@ export const Container = styled.div`
     flex-direction: column;
     gap: 2rem;
     padding: 2rem;
+    background: #fffae8;
 
     > ul {
         display: flex;
@@ -17,6 +18,11 @@ export const Container = styled.div`
             justify-content: space-between;
             box-shadow: 0 0 8px 0px #82828282;
             padding: 1rem;
+            background: cornsilk;
+
+            > p:last-of-type.limit {
+                color: red;
+            }
         }
     }
     
