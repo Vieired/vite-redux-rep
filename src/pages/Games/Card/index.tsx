dedf68da900cc1144fa85fe45c727c9dce9d2cab
Ednaldo Gomes <egomes@metatron.com.br>
2025-04-12 10:58:07 -0300

    fix(games): corrige contagem de dias em períodos que envolvem mêses e dias

--- src/pages/Games/Card/index.tsx
+++ src/pages/Games/Card/index.tsx
@@ -65,26 +65,27 @@ const Card: React.FC<Props> = ({
         const years = Math.floor(diffDays / 365);
         // const years = Math.floor(diffMonths / 12);
 
-        if(diffDays < 30) { // dias
+        if(diffDays < 30) { // Apenas dias
             return days > 0 ? `${Math.floor(diffDays)} dia${days > 1 ? "s" : ""}` : "";
         }
 
-        if(diffMonths < 12) { // meses
+        if(diffMonths < 12) { // Meses
+            const remainingDays = diffDays > 365 ? diffDays % 365 : (diffDays - months*30);
             const descriptionOfMonths = months > 0 ? `${months} mes${months > 1 ? "es" : ""}` : "";
-            const descriptionOfDays = days > 0 ? `${days} dia${days > 1 ? "s" : ""}` : "";
+            const descriptionOfDays = remainingDays > 0 ? `${remainingDays} dia${remainingDays > 1 ? "s" : ""}` : "";
             return descriptionOfMonths !== "" && descriptionOfDays !== ""
-                ? `${descriptionOfMonths} e ${descriptionOfDays}`
+                ? `${descriptionOfMonths} e ${descriptionOfDays} (${diffDays} dias)`
                 : descriptionOfMonths === "" && descriptionOfDays !== ""
-                    ? `${descriptionOfDays}`
-                    : `${descriptionOfMonths}`;
+                    ? `${descriptionOfDays} (${diffDays} dias)`
+                    : `${descriptionOfMonths} (${diffDays} dias)`;
         }
 
-        // mais de um ano
+        // Mais de um ano
         // const restDays = diffDays > 365 ? diffDays % 365 : days;
         const descriptionOfYears = years > 0 ? `${years} ano${years > 1 ? "s" :""}` : "";
         const descriptionOfMonths = months > 0 ? `${months} mes${months > 1 ? "es" : ""}` : "";
         const descriptionOfDays = days > 0 ? ` e ${days} dia${days > 1 ? "s" : ""}` : "";
-        return `${descriptionOfYears} ${descriptionOfMonths} ${descriptionOfDays}`
+        return `${descriptionOfYears} ${descriptionOfMonths} ${descriptionOfDays} (${diffDays} dias)`
     }
 
     const checkLimit = (startDate: string): boolean => {
