3fc83c615d46a2e588649c0efb3f6708c3a9c26b
Ednaldo Gomes <egomes@metatron.com.br>
2025-06-03 18:02:58 -0300

    fix: exibe feedback de confirmação ao alterar Configurações

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
 import { useFormik } from "formik";
 import Button from "../../components/Inputs/Button";
-import { Container, Content } from "./styles";
-import { useDispatch, useSelector } from "react-redux";
 import { InitialStateGames } from "../../shared/models/Games";
 import { selectGames, setLimitInMonths } from "../../store/gamesSlice";
 import InputNumber from "../../components/Inputs/InputNumber";
+import { Container, Content } from "./styles";
 
 
 const Settings: React.FC = () => {
@@ -18,6 +19,12 @@ const Settings: React.FC = () => {
     const handleSubmit = (data: { limitInMonths: number }) => {
         // TODO: futuramente, guardar este valor na base de dados
         dispatch(setLimitInMonths(data.limitInMonths));
+
+        toast.success("Frequência de limpezas alterada com sucesso.", {
+            toastId: "notification-message",
+        });
+
+        navigate('/');
     }
 
     const formik = useFormik({
@@ -43,6 +50,8 @@ const Settings: React.FC = () => {
                 <form onSubmit={formik.handleSubmit}>
                     <InputNumber
                         name="limitInMonths"
+                        label="Frequência de Limpeza (meses)"
+                        placeholder="Exemplo: 6"
                         value={formik.values.limitInMonths}
                         onChange={formik.handleChange}
                     />
