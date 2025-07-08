d279b02075a97060e7341c1a5306c435e3df2157
Ednaldo Gomes <egomes@metatron.com.br>
2025-07-08 20:04:03 -0300

    build: instala bibliotecas react-testing-library e vitest para testes automatizados

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [react()],
-})
+});
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    // setupFiles: "./vitest-setup.js",
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
+
+
+
+
+// /// <reference types="vitest" />
+// /// <reference types="vite/client" />
+// /// <reference types="vitest/config" />
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   test: {
+//     globals: true,
+//     environment: "jsdom",
+//   },
+//   plugins: [react()], 
+//   // test: {
+//   //     globals: true,
+//   //     environment: 'jsdom',
+// //   //     setupFiles: ['@testing-library/jest-dom'],
+// //   //     setupFiles: "./vitest-setup.js",
+//   // },
+// })
