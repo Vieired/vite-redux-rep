2a28e15277a4c73de0b0af36b64a17cb5012f0a5
Ednaldo Gomes <egomes@metatron.com.br>
2025-08-18 20:05:25 -0300

    test(login): aumenta cobertura de testes

--- src/pages/Login/index.test.tsx
+++ src/pages/Login/index.test.tsx
@@ -1,18 +1,58 @@
 import '@testing-library/jest-dom/vitest';
-import { render, screen } from '@testing-library/react';
+import {
+    fireEvent,
+    render,
+    screen,
+    waitFor,
+} from '@testing-library/react';
 import {
     describe,
     expect,
-    it,
-    // test,
+    // it,
+    test,
+    vi,
 } from 'vitest' ;
 import Login from '.';
 import { Provider } from 'react-redux';
 import store from '../../store/store';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+// import usersReducer from '../../store/usersSlice'
+// import { configureStore } from '@reduxjs/toolkit';
+
+// vi.mock("firebase/auth", () => ({
+//     signInWithEmailAndPassword: vi.fn()
+// }));
+
+// const submitMock = vi.fn();
+
+// const mockSignInWithEmailAndPasswordFn = vi.fn(signInWithEmailAndPassword);
+    // .mockImplementation(async () => {
+    //     return {}
+    // });
+
+vi.mock("firebase/auth", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const actual = await vi.importActual<any>("firebase/auth"); // importa o módulo original
+    return {
+        ...actual,
+        signInWithEmailAndPassword: vi.fn()
+    };
+});
 
 describe("Testa a página Login", () => {
 
-    it('Deveria renderizar "Login" no componente Login', async () => {
+    // const renderWithStore = (ui: React.ReactNode) => {
+    //     const store = configureStore({ reducer: { auth: usersReducer } });
+    //     return render(<Provider store={store}>{ui}</Provider>);
+    // };
+
+    // vi.mock('firebase/auth', () => ({
+    //     signInWithEmailAndPassword() {
+    //         return submitMock;
+    //     }
+    // }))
+
+    test('Deve renderizar o texto "Login" no componente Login', async () => {
 
         render(
             <Provider store={store}>
@@ -27,4 +67,63 @@ describe("Testa a página Login", () => {
 
         expect(screen.getByText("Login")).toBeInTheDocument();
     })
+
+    test("Deve chamar signInWithEmailAndPassword com email e senha", async () => {
+
+        (signInWithEmailAndPassword as ReturnType<typeof vi.fn>).mockResolvedValue({
+            user: { uid: "123", email: "teste@email.com" }
+        });
+
+        // renderWithStore(<Login />);
+        render(
+            <Provider store={store}>
+                <Login />
+            </Provider>
+        );
+
+        fireEvent.change(screen.getByPlaceholderText(/e-mail/i), {
+            target: { value: "teste@email.com" }
+        });
+        fireEvent.change(screen.getByPlaceholderText(/password/i), {
+            target: { value: "senha123" }
+        });
+        fireEvent.click(screen.getByRole("button", { name: /entrar/i }));
+
+        await waitFor(() => {
+            expect(signInWithEmailAndPassword).toHaveBeenCalledWith(
+                expect.anything(), // o auth
+                "teste@email.com",
+                "senha123"
+            );
+        });
+    });
+
+    test("Deve falhar a chamada signInWithEmailAndPassword com email e senha", async () => {
+
+        (signInWithEmailAndPassword as ReturnType<typeof vi.fn>).mockRejectedValue({
+            user: { uid: "123", email: "teste@email.com" }
+        });
+
+        render(
+            <Provider store={store}>
+                <Login />
+            </Provider>
+        );
+
+        fireEvent.change(screen.getByPlaceholderText(/e-mail/i), {
+            target: { value: "teste@email.com" }
+        });
+        fireEvent.change(screen.getByPlaceholderText(/password/i), {
+            target: { value: "senha123" }
+        });
+        fireEvent.click(screen.getByRole("button", { name: /entrar/i }));
+
+        await waitFor(() => {
+            expect(signInWithEmailAndPassword).toHaveBeenCalledWith(
+                expect.anything(), // o auth
+                "teste@email.com",
+                "senha123"
+            );
+        });
+    });
 });
