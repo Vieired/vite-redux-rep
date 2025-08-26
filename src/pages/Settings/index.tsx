ade0dc5ebd8104956be769a0b811b5890730d71e
Ednaldo Gomes <egomes@metatron.com.br>
2025-08-26 10:22:28 -0300

    fix(settings): remove chamada desnecessária do fetchGames após response do updateSettings

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -5,7 +5,7 @@ import { FaArrowLeft as LeftIcon, FaCheck } from 'react-icons/fa';
 import { useFormik } from "formik";
 import Button from "../../components/Inputs/Button";
 import { InitialStateGames } from "../../shared/models/Games";
-import { fetchGames, selectGames, updateSettings } from "../../store/gamesSlice";
+import { selectGames, updateSettings } from "../../store/gamesSlice";
 import InputNumber from "../../components/Inputs/InputNumber";
 import { Container, Content, Buttons } from "./styles";
 
@@ -15,7 +15,7 @@ const Settings: React.FC = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const { showOnlyActiveGamesFilter } = useSelector(selectGames);
+    // const { showOnlyActiveGamesFilter } = useSelector(selectGames);
     const gamesStore = useSelector(selectGames) as InitialStateGames;
 
     const handleSubmit = (data: { limitInMonths: number }) => {
@@ -24,11 +24,11 @@ const Settings: React.FC = () => {
 
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         dispatch(updateSettings({cleaningFrequency: data.limitInMonths}) as any)
-            .then(() => {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                dispatch(fetchGames(showOnlyActiveGamesFilter) as any);
-                // dispatch(fetchSettings() as any);
-            })
+            // .then(() => {
+            //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            //     dispatch(fetchGames(showOnlyActiveGamesFilter) as any);
+            //     // dispatch(fetchSettings() as any);
+            // })
 
         toast.success("Frequência de limpezas alterada com sucesso.", {
             toastId: "notification-message",
